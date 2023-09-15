ea73074bb1723da001962595fc24d981390df7c5
AMaherJK <AMaherJK@GMail.com>
2023-09-15 05:54:38 +0300

    5th & 6th tasks

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,6 +11,3 @@ void print_most_numbers(void);
 void more_numbers(void);
 
 #endif 
-/* 
-Bwave ICT 
-*/
