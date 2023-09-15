8482a199c74375957b7923b58e9b98ebd38ed377
AMaherJK <AMaherJK@GMail.com>
2023-09-15 05:49:04 +0300

    3rd & 4th tasks

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,14 @@
 
 /**
 *print_numbers - print 0123456789
-* owned by Bwave ICT
 *Return: void
 */
 
 void print_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
 	_putchar(c);
 	}
